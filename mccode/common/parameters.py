b2c4a4016d2a65be4e198375ed97b12166e93449
Gregory Tucker <gregory.tucker@ess.eu>
2023-07-18 13:22:17 +0200

    [Add] more extensive partially evalualted value support

--- mccode/common/parameters.py
+++ mccode/common/parameters.py
@@ -97,6 +97,32 @@ class Value:
             return Value(self.data_type, f'{fname}({self.value})')
         return Value._handle_return_value(f(self.value))
 
+    @staticmethod
+    def apply_elementwise_binary_function(f, fname: str, va, vb):
+        ta = va.data_type if hasattr(va, 'data_type') else value_type(str(type(va)))
+        tb = vb.data_type if hasattr(vb, 'data_type') else value_type(str(type(vb)))
+        a = va.value if hasattr(va, 'value') else va
+        b = vb.value if hasattr(vb, 'value') else vb
+        if a is None or b is None:
+            raise RuntimeError(r"Can not calculate {fname} for uninitialized value(s)")
+        if ta == Value.Type.str or tb == Value.Type.str:
+            return Value.str(f'{fname}({a}, {b})')
+        # convert singular arrays to scalars
+        a_has, b_has = [hasattr(x, '__len__') for x in (a, b)]
+        a, a_has = (a[0], False) if a_has and len(a) == 1 else (a, a_has)
+        b, b_has = (b[0], False) if b_has and len(b) == 1 else (b, b_has)
+        if a_has and b_has and len(a) != len(b):
+            raise RuntimeError(f"Elementwise binary operations require same-sized arrays")
+        elif a_has and b_has:
+            v = [f(i, j) for i, j in zip(a, b)]
+        elif a_has:
+            v = [f(a, j) for j in b]
+        elif b_has:
+            v = [f(i, b) for i in a]
+        else:
+            v = f(a, b)
+        return Value._handle_return_value(v)
+
     def _binary_op(self, v, fun, op):
         rv = self.apply_elementwise_function(fun, op)
         if rv.is_a(Value.Type.str):
@@ -142,6 +168,20 @@ class Value:
             return Value(Value.Type.str, f'abs({self.value})')
         return self.apply_elementwise_function(lambda x: abs(x), 'abs')
 
+    def __gt__(self, other):
+        if self.data_type == Value.Type.str or hasattr(other, 'data_type') and other.data_type == Value.Type.str:
+            return Value(Value.Type.str, f'{self.value} > {other}')
+        if hasattr(other, 'data_type'):
+            other = other.value
+        return Value(Value.Type.int, 1 if self.value > other else 0)
+
+    def __lt__(self, other):
+        if self.data_type == Value.Type.str or hasattr(other, 'data_type') and other.data_type == Value.Type.str:
+            return Value(Value.Type.str, f'{self.value} <>> {other}')
+        if hasattr(other, 'data_type'):
+            other = other.value
+        return Value(Value.Type.int, 1 if self.value < other else 0)
+
 
 @dataclass
 class InstrumentParameter:
@@ -171,3 +211,13 @@ class ComponentParameter:
 def parameter_name_present(parameters: tuple[Union[InstrumentParameter, ComponentParameter]],
                            parameter: Union[InstrumentParameter, ComponentParameter]) -> bool:
     return any(parameter.name == x.name for x in parameters)
+
+
+def value_type(s: str):
+    if s == 'str':
+        return Value.Type.str
+    elif s == 'float' or s == 'double':
+        return Value.Type.float
+    elif s == 'int':
+        return Value.Type.int
+    raise RuntimeError(f'How do I handle {s} to Value.Type conversion?')
--- mccode/instance/orientation.py
+++ mccode/instance/orientation.py
@@ -16,11 +16,17 @@ def sin_value(v: Value):
     return v.apply_elementwise_function(sin, 'sin')
 
 
+def atan2_value(va: Value, vb: Value):
+    from math import atan2
+    return Value.apply_elementwise_binary_function(atan2, 'atan2', va, vb)
+
+
 def degree_to_radian(v: Value):
     from math import pi
     if v.is_a(Value.Type.str):
         return v * Value.str('PI/180')
-    return v * (pi/180)
+    return v * (pi / 180)
+
 
 @dataclass
 class Orientation:
@@ -32,24 +38,43 @@ class Orientation:
     def affine_matrix(self):
         m = ((self.rotation[0], self.rotation[1], self.rotation[2], self.position[0]),
              (self.rotation[3], self.rotation[4], self.rotation[5], self.position[1]),
-             (self.rotation[6], self.rotation[7], self.rotation[9], self.position[2]),
+             (self.rotation[6], self.rotation[7], self.rotation[8], self.position[2]),
              (Value.float(0), Value.float(0), Value.float(0), Value.float(1)))
         return m
 
     @property
     def angles(self):
-        # Copied from eniius get_euler_angles?
-        pass
+        def abs_ch(a, b):
+            v = abs(a) > abs(b)
+            return v.value == 1 if isinstance(v, Value) else v
 
+        # Inspired by eniius get_euler_angles
+        x = atan2_value(-self.rotation[7], self.rotation[8])  # tan(x) = sin(x) / cos(x) = -(-sx*cy)/(cx*cy)
+        z = atan2_value(-self.rotation[3], self.rotation[0])  # tan(z) = sin(z) / cos(z) = -(-cy*sz)/(cy*cz)
+        # Ensure we know the sign of cos(y), not just its magnitude:
+        cos_x, cos_z = [cos_value(a) for a in (x, z)]
+        if not x.is_a(Value.Type.str) and not z.is_a(Value.Type.str):
+            if abs_ch(cos_x, cos_z):
+                cos_a, sin_a, cos_y_cos_a, sin_y_sin_a = cos_x, sin_value(x), self.rotation[8], -self.rotation[7]
+            else:
+                cos_a, sin_a, cos_y_cos_a, sin_y_sin_a = cos_z, sin_value(z), self.rotation[0], -self.rotation[3]
+        elif not x.is_a(Value.Type.str):
+            cos_a, sin_a, cos_y_cos_a, sin_y_sin_a = cos_x, sin_value(x), self.rotation[8], -self.rotation[7]
+        elif not z.is_a(Value.Type.str):
+            cos_a, sin_a, cos_y_cos_a, sin_y_sin_a = cos_z, sin_value(z), self.rotation[0], -self.rotation[3]
+        else:
+            raise RuntimeError("How do we prevent division by zero when both x and z are strings?")
+        y = atan2_value(self.rotation[6], cos_y_cos_a / cos_a if abs_ch(cos_a, sin_a) else sin_y_sin_a / sin_a)
+        return x, y, z
 
     @staticmethod
     def from_at_rotated(at, rotated):
         cx, cy, cz = [cos_value(degree_to_radian(r)) for r in rotated]
         sx, sy, sz = [sin_value(degree_to_radian(r)) for r in rotated]
         # The 3x3 rotation matrix part:
-        m = ( cy * cz, sx * sy * cz + cx * sz, sx * sz - cx * sy * cz,
+        m = (cy * cz, sx * sy * cz + cx * sz, sx * sz - cx * sy * cz,
              -cy * sz, cx * cz - sx * sy * sz, sx * cz + cx * sy * sz,
-              sy,     -sx * cy,                cx * cy)
+             sy, -sx * cy, cx * cy)
         # The 3 vector part:
         v = at[0], at[1], at[2]
         return Orientation(v, m)
@@ -59,10 +84,15 @@ def from_at_rotated(at: tuple[Value, Value, Value], rotated: tuple[Value, Value,
     return Orientation.from_at_rotated(at, rotated)
 
 
-def from_at_relative_rotated_relative(at: tuple[float, float, float], at_relative: Orientation,
-                                      rotated: tuple[float, float, float], rotated_relative: Orientation):
+def from_at_relative_rotated_relative(at: tuple[Value, Value, Value], at_relative: Orientation,
+                                      rotated: tuple[Value, Value, Value], rotated_relative: Orientation):
     rat = at_relative.position
     global_at = at[0] + rat[0], at[1] + rat[1], at[2] + rat[2]
-    rrot = rotated_relative.rotation
-    global_rot = rotated[0] + rrot[0], rotated[1] + rrot[1], rotated[2] + rrot[2]
-    return Orientation(global_at, global_rot)
+    rot_rel = rotated_relative.angles
+    global_rot = rotated[0] + rot_rel[0], rotated[1] + rot_rel[1], rotated[2] + rot_rel[2]
+    return Orientation.from_at_rotated(global_at, global_rot)
+
+
+def from_at_rotated_relative(at: tuple[Value, Value, Value], rotated: tuple[Value, Value, Value],
+                             relative: Orientation):
+    return from_at_relative_rotated_relative(at, relative, rotated, relative)
