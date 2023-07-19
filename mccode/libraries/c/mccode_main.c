34f9421912ce3a1fc50a3a7a75d0df96ad259ddf
Gregory Tucker <gregory.tucker@gmail.com>
2023-07-19 22:06:49 +0200

    [Add] McStas runtime sources, to start

---
+++ mccode/libraries/c/mccode_main.c
@@ -0,0 +1,189 @@
+/*******************************************************************************
+* mccode_main: McCode main() function.
+*******************************************************************************/
+int mccode_main(int argc, char *argv[])
+{
+  /*  double run_num = 0; */
+  time_t  t;
+  clock_t ct;
+
+#ifdef USE_MPI
+  char mpi_node_name[MPI_MAX_PROCESSOR_NAME];
+  int  mpi_node_name_len;
+#endif /* USE_MPI */
+
+#ifdef MAC
+  argc = ccommand(&argv);
+#endif
+
+#ifdef USE_MPI
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_node_count); /* get number of nodes */
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_node_rank);
+  MPI_Comm_set_name(MPI_COMM_WORLD, instrument_name);
+  MPI_Get_processor_name(mpi_node_name, &mpi_node_name_len);
+#endif /* USE_MPI */
+
+  ct = clock();
+
+  // device and host functional RNG seed
+  struct timeval tm;
+  gettimeofday(&tm, NULL);
+  mcseed = (long) tm.tv_sec*1000000 + tm.tv_usec;
+  mcstartdate = (long)tm.tv_sec;  /* set start date before parsing options and creating sim file */
+  // init global _particle.randstate for random number use
+  // during init(), finally() and display(). NOTE: during trace, a local
+  // "_particle" variable is present and thus used instead.
+  srandom(_hash(mcseed-1));
+
+#ifdef USE_MPI
+  /* *** print number of nodes *********************************************** */
+  if (mpi_node_count > 1) {
+    MPI_MASTER(
+    printf("Simulation '%s' (%s): running on %i nodes (master is '%s', MPI version %i.%i).\n",
+      instrument_name, instrument_source, mpi_node_count, mpi_node_name, MPI_VERSION, MPI_SUBVERSION);
+    );
+    /* share the same seed, then adapt random seed for each node */
+    MPI_Bcast(&mcseed, 1, MPI_LONG, 0, MPI_COMM_WORLD); /* root sends its seed to slaves */
+    mcseed += mpi_node_rank; /* make sure we use different seeds per noe */
+  }
+#endif /* USE_MPI */
+
+#ifdef OPENACC
+#ifdef USE_MPI
+  int num_devices = acc_get_num_devices(acc_device_nvidia);
+  if(num_devices>0){
+    int my_device = mpi_node_rank % num_devices;
+    acc_set_device_num( my_device, acc_device_nvidia );
+    printf("Have found %d GPU devices on rank %d. Will use device %d.\n", num_devices, mpi_node_rank, my_device);
+  }else{
+    printf("There was an issue probing acc_get_num_devices, fallback to host\n");
+    acc_set_device_type( acc_device_host );
+  }
+#endif
+#endif
+
+  /* *** parse options ******************************************************* */
+  SIG_MESSAGE("[" __FILE__ "] main START");
+  mcformat = getenv(FLAVOR_UPPER "_FORMAT") ?
+             getenv(FLAVOR_UPPER "_FORMAT") : FLAVOR_UPPER;
+  instrument_exe = argv[0]; /* store the executable path */
+  /* read simulation parameters and options */
+  mcparseoptions(argc, argv); /* sets output dir and format */
+
+
+#ifdef USE_MPI
+  if (mpi_node_count > 1) {
+    /* share the same seed, then adapt random seed for each node */
+    MPI_Bcast(&mcseed, 1, MPI_LONG, 0, MPI_COMM_WORLD); /* root sends its seed to slaves */
+    mcseed += mpi_node_rank; /* make sure we use different seeds per node */
+  }
+#endif
+
+
+/* *** install sig handler, but only once !! after parameters parsing ******* */
+#ifndef NOSIGNALS
+#ifdef SIGQUIT
+  if (signal( SIGQUIT ,sighandler) == SIG_IGN)
+    signal( SIGQUIT,SIG_IGN);   /* quit (ASCII FS) */
+#endif
+#ifdef SIGABRT
+  if (signal( SIGABRT ,sighandler) == SIG_IGN)
+    signal( SIGABRT,SIG_IGN);   /* used by abort, replace SIGIOT in the future */
+#endif
+#ifdef SIGTERM
+  if (signal( SIGTERM ,sighandler) == SIG_IGN)
+    signal( SIGTERM,SIG_IGN);   /* software termination signal from kill */
+#endif
+#ifdef SIGUSR1
+  if (signal( SIGUSR1 ,sighandler) == SIG_IGN)
+    signal( SIGUSR1,SIG_IGN);   /* display simulation status */
+#endif
+#ifdef SIGUSR2
+  if (signal( SIGUSR2 ,sighandler) == SIG_IGN)
+    signal( SIGUSR2,SIG_IGN);
+#endif
+#ifdef SIGHUP
+  if (signal( SIGHUP ,sighandler) == SIG_IGN)
+    signal( SIGHUP,SIG_IGN);
+#endif
+#ifdef SIGILL
+  if (signal( SIGILL ,sighandler) == SIG_IGN)
+    signal( SIGILL,SIG_IGN);    /* illegal instruction (not reset when caught) */
+#endif
+#ifdef SIGFPE
+  if (signal( SIGFPE ,sighandler) == SIG_IGN)
+    signal( SIGSEGV,SIG_IGN);    /* floating point exception */
+#endif
+#ifdef SIGBUS
+  if (signal( SIGBUS ,sighandler) == SIG_IGN)
+    signal( SIGSEGV,SIG_IGN);    /* bus error */
+#endif
+#ifdef SIGSEGV
+  if (signal( SIGSEGV ,sighandler) == SIG_IGN)
+    signal( SIGSEGV,SIG_IGN);   /* segmentation violation */
+#endif
+#endif /* !NOSIGNALS */
+
+
+  // init executed by master/host
+  siminfo_init(NULL); /* open SIM */
+  SIG_MESSAGE("[" __FILE__ "] main INITIALISE");
+  init();
+
+
+#ifndef NOSIGNALS
+#ifdef SIGINT
+  if (signal( SIGINT ,sighandler) == SIG_IGN)
+    signal( SIGINT,SIG_IGN);    /* interrupt (rubout) only after INIT */
+#endif
+#endif /* !NOSIGNALS */
+
+/* ================ main particle generation/propagation loop ================ */
+#ifdef USE_MPI
+  /* sliced Ncount on each MPI node */
+  mcncount = mpi_node_count > 1 ?
+    floor(mcncount / mpi_node_count) :
+    mcncount; /* number of rays per node */
+#endif
+
+// MT specific init, note that per-ray init is empty
+#if RNG_ALG == 2
+  mt_srandom(mcseed);
+#endif
+
+
+// main raytrace work loop
+#ifndef FUNNEL
+  // legacy version
+  raytrace_all(mcncount, mcseed);
+#else
+  MPI_MASTER(
+  // "funneled" version in which propagation is more parallelizable
+  printf("\nNOTE: CPU COMPONENT grammar activated:\n 1) \"FUNNEL\" raytrace algorithm enabled.\n 2) Any SPLIT's are dynamically allocated based on available buffer size. \n");
+	     );
+  raytrace_all_funnel(mcncount, mcseed);
+#endif
+
+
+#ifdef USE_MPI
+ /* merge run_num from MPI nodes */
+  if (mpi_node_count > 1) {
+  double mcrun_num_double = (double)mcrun_num;
+  mc_MPI_Sum(&mcrun_num_double, 1);
+  mcrun_num = (unsigned long long)mcrun_num_double;
+  }
+#endif
+
+
+  // save/finally executed by master node/thread/host
+  finally();
+
+
+#ifdef USE_MPI
+  MPI_Finalize();
+#endif /* USE_MPI */
+
+
+  return 0;
+} /* mccode_main */
---
+++ mccode/libraries/c/r-interoff-lib.h
@@ -0,0 +1,216 @@
+/*******************************************************************************
+*
+* McStas, neutron ray-tracing package
+*         Copyright (C) 1997-2008, All rights reserved
+*         Risoe National Laboratory, Roskilde, Denmark
+*         Institut Laue Langevin, Grenoble, France
+*
+* Runtime: share/interoff.h
+*
+* %Identification
+* Written by: Reynald Arnerin
+* Date:    Jun 12, 2008
+* Release: 
+* Version: 
+*
+* Object File Format intersection header for McStas. Requires the qsort function.
+*
+* Such files may be obtained with e.g.
+*   qhull < points.xyz Qx Qv Tv o > points.off
+* where points.xyz has format:
+*   3
+*   <nb_points>
+*   <x> <y> <z>
+*   ...
+* The resulting file should have its first line being changed from '3' into 'OFF'.
+* It can then be displayed with geomview.
+* A similar, but somewhat older solution is to use 'powercrust' with e.g.
+*   powercrust -i points.xyz
+* which will generate a 'pc.off' file to be renamed as suited.
+*
+*******************************************************************************/
+
+#ifndef R_INTEROFF_LIB_H
+#define R_INTEROFF_LIB_H "$Revision$"
+
+#ifndef OFF_EPSILON
+#define OFF_EPSILON 1e-13
+#endif
+
+#ifndef OFF_INTERSECT_MAX
+#ifdef OPENACC
+#define OFF_INTERSECT_MAX 100
+#else
+#define OFF_INTERSECT_MAX 1024
+#endif
+#endif
+
+//#include <float.h>
+
+#define N_VERTEX_DISPLAYED    200000
+
+typedef struct r_intersection {
+	MCNUM time;  	  //time of the intersection
+	Coords v;	      //intersection point
+	Coords normal;  //normal vector of the surface intersected
+	short in_out;	  //1 if the ray enters the volume, -1 otherwise
+	short edge;	    //1 if the intersection is on the boundary of the polygon, and error is possible
+	unsigned long index; // index of the face
+} r_intersection;
+
+typedef struct r_polygon {
+  MCNUM* p;       //vertices of the polygon in adjacent order, this way : x1 | y1 | z1 | x2 | y2 | z2 ...
+  int npol;       //number of vertices
+  #pragma acc shape(p[0:npol]) init_needed(npol)
+  Coords normal;
+  double D;
+} r_polygon;
+
+typedef struct r_off_struct {
+    long vtxSize;
+    long polySize;
+    long faceSize;
+    Coords* vtxArray;
+    #pragma acc shape(vtxArray[0:vtxSize]) init_needed(vtxSize)
+    Coords* normalArray;
+    #pragma acc shape(vtxArray[0:faceSize]) init_needed(faceSize)
+    unsigned long* faceArray;
+    #pragma acc shape(vtxArray[0:faceSize][0:polySize]) init_needed(faceSize,polySize)
+    // GM: Additions to store floating point based values for m, alpha and W, for each polygon face
+    double* face_m_Array;
+    #pragma acc shape(face_m_Array[0:faceSize]) init_needed(faceSize)
+    double* face_alpha_Array;
+    #pragma acc shape(face_alpha_Array[0:faceSize]) init_needed(faceSize)
+    double* face_W_Array;
+    double* DArray;
+    #pragma acc shape(face_W_Array[0:faceSize]) init_needed(faceSize)
+
+    char *filename;
+    int mantidflag;
+    long mantidoffset;
+    r_intersection intersects[OFF_INTERSECT_MAX]; // After a call to r_off_intersect_all contains the list of intersections.
+    int nextintersect;                 // 'Next' intersection (first t>0) solution after call to r_off_intersect_all
+    int numintersect;               // Number of intersections after call to r_off_intersect_all
+} r_off_struct;
+
+/*******************************************************************************
+* long r_off_init(  char *offfile, double xwidth, double yheight, double zdepth, r_off_struct* data)
+* ACTION: read an OFF file, optionally center object and rescale, initialize OFF data structure
+* INPUT: 'offfile' OFF file to read
+*        'xwidth,yheight,zdepth' if given as non-zero, apply bounding box. 
+*           Specifying only one of these will also use the same ratio on all axes
+*        'notcenter' center the object to the (0,0,0) position in local frame when set to zero
+* RETURN: number of polyhedra and 'data' OFF structure 
+*******************************************************************************/
+long r_off_init(  char *offfile, double xwidth, double yheight, double zdepth, 
+                int notcenter, r_off_struct* data);
+
+/*******************************************************************************
+* int r_off_intersect_all(double* t0, double* t3, 
+     Coords *n0, Coords *n3,
+     unsigned long *fi0, unsigned long *fi3,
+     double x, double y, double z,
+     double vx, double vy, double vz,
+     double ax, double ay, double az,
+     r_off_struct *data )
+* ACTION: computes intersection of neutron trajectory with an object. 
+* INPUT:  x,y,z and vx,vy,vz are the position and velocity of the neutron
+*         ax, ay, az are the local acceleration vector
+*         data points to the OFF data structure
+* RETURN: the number of polyhedra which trajectory intersects
+*         t0 and t3 are the smallest incoming and outgoing intersection times
+*         n0 and n3 are the corresponding normal vectors to the surface
+*         data is the full OFF structure, including a list intersection type
+*******************************************************************************/
+#pragma acc routine
+int r_off_intersect_all(double* t0, double* t3, 
+     Coords *n0, Coords *n3,
+     unsigned long *faceindex0, unsigned long *faceindex3,
+     double x, double y, double z,
+     double vx, double vy, double vz,
+     double ax, double ay, double az,
+     r_off_struct *data );
+
+/*******************************************************************************
+* int r_off_intersect(double* t0, double* t3, 
+     Coords *n0, Coords *n3,
+     unsigned long *faceindex0, unsigned long *faceindex3,
+     double x, double y, double z,
+     double vx, double vy, double vz,
+     double ax, double ay, double az,
+     r_off_struct data )
+* ACTION: computes intersection of neutron trajectory with an object. 
+* INPUT:  x,y,z and vx,vy,vz are the position and velocity of the neutron
+*         ax, ay, az are the local acceleration vector
+*         data points to the OFF data structure
+* RETURN: the number of polyhedra which trajectory intersects
+*         t0 and t3 are the smallest incoming and outgoing intersection times
+*         n0 and n3 are the corresponding normal vectors to the surface
+*******************************************************************************/
+#pragma acc routine
+int r_off_intersect(double* t0, double* t3, 
+     Coords *n0, Coords *n3,
+     unsigned long *faceindex0, unsigned long *faceindex3,
+     double x, double y, double z, 
+     double vx, double vy, double vz, 
+     double ax, double ay, double az,
+     r_off_struct data );
+
+/*****************************************************************************
+* int r_off_intersectx(double* l0, double* l3, 
+     Coords *n0, Coords *n3,
+     unsigned long *faceindex0, unsigned long *faceindex3,
+     double x, double y, double z, 
+     double kx, double ky, double kz, 
+     r_off_struct data )
+* ACTION: computes intersection of an xray trajectory with an object.
+* INPUT:  x,y,z and kx,ky,kz, are spatial coordinates and wavevector of the x-ray
+*         respectively. data points to the OFF data structure.
+* RETURN: the number of polyhedra the trajectory intersects
+*         l0 and l3 are the smallest incoming and outgoing intersection lengths
+*         n0 and n3 are the corresponding normal vectors to the surface
+*******************************************************************************/
+#pragma acc routine
+int r_off_x_intersect(double *l0,double *l3,
+     Coords *n0, Coords *n3,
+     unsigned long *faceindex0, unsigned long *faceindex3,
+     double x,  double y,  double z, 
+     double kx, double ky, double kz, 
+     r_off_struct data );
+
+/*******************************************************************************
+* void r_off_display(r_off_struct data)
+* ACTION: display up to N_VERTEX_DISPLAYED points from the object
+*******************************************************************************/
+void r_off_display(r_off_struct);
+
+/*******************************************************************************
+void r_p_to_quadratic(double eq[], Coords acc,
+                    Coords pos, Coords vel,
+                    double* teq)
+* ACTION: define the quadratic for the intersection of a parabola with a plane
+* INPUT: 'eq' plane equation
+*        'acc' acceleration vector
+*        'vel' velocity of the particle
+*        'pos' position of the particle
+*         equation of plane A * x + B * y + C * z - D = 0
+*         eq[0] = (C*az)/2+(B*ay)/2+(A*ax)/2
+*         eq[1] = C*vz+B*vy+A*vx
+*         eq[2] = C*z0+B*y0+A*x0-D
+* RETURN: equation of parabola: teq(0) * t^2 + teq(1) * t + teq(2)
+*******************************************************************************/
+void r_p_to_quadratic(Coords norm, MCNUM d, Coords acc, Coords pos, Coords vel,
+		    double* teq);
+
+/*******************************************************************************
+int r_quadraticSolve(double eq[], double* x1, double* x2);
+* ACTION: solves the quadratic for the roots x1 and x2 
+*         eq[0] * t^2 + eq[1] * t + eq[2] = 0
+* INPUT: 'eq' the coefficients of the parabola
+* RETURN: roots x1 and x2 and the number of solutions
+*******************************************************************************/
+int r_quadraticSolve(double* eq, double* x1, double* x2);
+
+#endif
+
+/* end of r-interoff-lib.h */
