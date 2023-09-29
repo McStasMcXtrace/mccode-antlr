8a7cf48ee6a32ed14ee52404f98f296f974b1eb9
Gregory Tucker <gregory.tucker@ess.eu>
2023-09-29 14:30:42 +0200

    [Add] parsed instrument positioning test

--- test/test_instr.py
+++ test/test_instr.py
@@ -255,22 +255,19 @@ class TestInstr(TestCase):
         self.assertTrue(isinstance(d, Matrix))
         self.assertEqual(round(abs(d), 14), Matrix())
 
-    def test_assemble_positioning(self):
+    def _positioning_evaluator(self, instr):
+        """Common positioning checks for `test_read_positioning` and `test_assemble_positioning`"""
         from mccode.common import Expr
         from mccode.instr.orientation import Vector, Rotation
         z, o = Expr.float(0), Expr.float(1)
 
-        assembler = make_mcstas_assembler('orientation_test')
-        # Start the actual instrument:
-        assembler.component("origin", "Progress_bar", at=[0, 0, 0])
-        # 'Fun' Positioning stuff
-        left = assembler.component('left', 'Arm', at=([0, 0, 1], "origin"), rotate=[0, 90, 0])
+        left = instr.get_component('left')
         v3 = Vector(z, z, o)
         self.assertEqual(v3, left.orientation.position())
         self.assertEqual(Rotation(z, z, -o, z, o, z, o, z, z), left.orientation.rotation())
         self.assertEqual(left.orientation.rotation(), left.orientation.rotation('coordinates').inverse())
 
-        up = assembler.component("up", 'Arm', at=([0, 0, 1], "left"), rotate=[-90, 0, 0])
+        up = instr.get_component('up')
         v4 = Vector(o, z, z)
         self.assertEqual(v4, up.orientation.position() - left.orientation.position())
         up_orientation = Rotation(z, z, -o, -o, z, z, z, o, z)
@@ -278,21 +275,26 @@ class TestInstr(TestCase):
         self.assertEqual(Vector(o, z, o), up.orientation.position())
         self.assertRotationsEqual(up_orientation, up.orientation.rotation('coordinates').inverse())
 
-        last = assembler.component("last", 'Arm', at=([0, 0, 1], "up"), rotate=[0, 0, 0])
+        last = instr.get_component('last')
         v5 = Vector(z, o, z)
         self.assertRotationsEqual(up_orientation, last.orientation.rotation())
-        print(last.orientation.position())
-        print(up.orientation.position())
         self.assertEqual(v5, last.orientation.position() - up.orientation.position())
 
+    def test_assemble_positioning(self):
+        """Equivalent test to `test_read_positioning` but using an assembled instrument"""
+        assembler = make_mcstas_assembler('orientation_test')
+        origin = assembler.component("origin", "Progress_bar", at=[0, 0, 0])
+        left = assembler.component('left', 'Arm', at=([0, 0, 1], origin), rotate=[0, 90, 0])
+        up = assembler.component("up", 'Arm', at=([0, 0, 1], left), rotate=[-90, 0, 0])
+        assembler.component("last", 'Arm', at=([0, 0, 1], up), rotate=[0, 0, 0])
+        self._positioning_evaluator(assembler.instrument)
+
     def test_read_positioning(self):
-        instr_source = """
-        DEFINE INSTRUMENT orientation_test()
-        TRACE
+        """Equivalent test to `test_assemble_positioning` but using a parsed instrument"""
+        instr_source = """DEFINE INSTRUMENT orientation_test() TRACE
         COMPONENT origin = Arm() AT (0, 0, 0) ABSOLUTE
         COMPONENT left = Arm() AT (0, 0, 1) RELATIVE PREVIOUS ROTATED (0, 90, 0) RELATIVE origin
         COMPONENT up = Arm() AT (0, 0, 1) RELATIVE left ROTATED (-90, 0, 0) RELATIVE left
         COMPONENT last = Arm() AT (0, 0, 1) RELATIVE up
-        END
-        """
-        instr = parse_instr_string(instr_source)
+        END """
+        self._positioning_evaluator(parse_instr_string(instr_source))
