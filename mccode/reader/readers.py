bc61f2c687e4658de3a4b56a7ff757f1f0a3c50a
Gregory Tucker <gregory.tucker@ess.eu>
2023-10-02 09:35:29 +0200

    [Ref] Add McXtrace reader with common McCode reader function

--- mccode/reader/readers.py
+++ mccode/reader/readers.py
@@ -1,13 +1,25 @@
 from pathlib import Path
 from typing import Union
+from mccode.instr import Instr
+from mccode.reader import Registry
 
 
-def read_mcstas_instr(filename: Union[str, Path]):
+def read_mccode_instr(filename: Union[str, Path], registry: Registry) -> Instr:
     from antlr4 import CommonTokenStream, InputStream
     from mccode.grammar import McInstrParser, McInstrLexer
     from mccode.instr import InstrVisitor
-    from mccode.reader import MCSTAS_REGISTRY, Reader
+    from mccode.reader import Reader
     instr_source = Path(filename).read_text()
     parser = McInstrParser(CommonTokenStream(McInstrLexer(InputStream(instr_source))))
-    visitor = InstrVisitor(Reader(registries=[MCSTAS_REGISTRY]), str(filename))
+    visitor = InstrVisitor(Reader(registries=[registry]), str(filename))
     return visitor.visitProg(parser.prog())
+
+
+def read_mcstas_instr(filename: Union[str, Path]) -> Instr:
+    from mccode.reader import MCSTAS_REGISTRY
+    return read_mccode_instr(filename, MCSTAS_REGISTRY)
+
+
+def read_mcxtrace_instr(filename: Union[str, Path]) -> Instr:
+    from mccode.reader import MCXTRACE_REGISTRY
+    return read_mccode_instr(filename, MCXTRACE_REGISTRY)
