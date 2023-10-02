d8d8727373aadc9f5e07263d4246eb5910d1f72a
Gregory Tucker <gregory.tucker@ess.eu>
2023-10-02 09:40:54 +0200

    [Fix] McStas loader import

--- mccode/loader/__init__.py
+++ mccode/loader/__init__.py
@@ -1,6 +1,6 @@
-from loader import load_mccode_instr, load_mcxtrace_instr
+from loader import load_mcstas_instr, load_mcxtrace_instr
 
 __all__ = [
-    'load_mccode_instr',
+    'load_mcstas_instr',
     'load_mcxtrace_instr'
 ]
