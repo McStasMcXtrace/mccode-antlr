b121037fe8cdb97aa98685534c806d9ebe7245ab
Gregory Tucker <gregory.tucker@ess.eu>
2023-10-09 09:41:40 +0200

    [Fix] instr copy error, [Add] copy instr test

--- mccode/instr/instr.py
+++ mccode/instr/instr.py
@@ -322,17 +322,19 @@ class Instr:
     def copy(self, first=0, last=-1):
         """Return a copy of this instrument, optionally with only a subset of components"""
         copy = Instr(self.name, self.source)
-        copy.parameters = (x for x in self.parameters)
-        copy.metadata = (x for x in self.metadata)
-        copy.components = (x for x in self.components[first:last])
-        copy.included = (x for x in self.included)
-        copy.user = (x for x in self.user)
-        copy.declare = (x for x in self.declare)
-        copy.initialize = (x for x in self.initialize)
-        copy.save = (x for x in self.save)
-        copy.final = (x for x in self.final)
-        copy.flags = (x for x in self.flags)
-        copy.registries = (x for x in self.registries)
+        copy.parameters = tuple(x for x in self.parameters)
+        copy.metadata = tuple(x for x in self.metadata)
+        if last < 0:
+            last += 1 + len(self.components)
+        copy.components = tuple(x for x in self.components[first:last])
+        copy.included = tuple(x for x in self.included)
+        copy.user = tuple(x for x in self.user)
+        copy.declare = tuple(x for x in self.declare)
+        copy.initialize = tuple(x for x in self.initialize)
+        copy.save = tuple(x for x in self.save)
+        copy.final = tuple(x for x in self.final)
+        copy.flags = tuple(x for x in self.flags)
+        copy.registries = tuple(x for x in self.registries)
         return copy
 
     def split(self, after):
@@ -352,7 +354,7 @@ class Instr:
         if filename is None:
             filename = self.name + '.mcpl'
         first, second = self.split(after)
-        first.parameters += (InstrumentParameter('mcpl_filename', 'string', filename),)
+        first.add_parameter(InstrumentParameter('mcpl_filename', 'string', filename))
         fc = first.components[-1]
         if fc.type != 'Arm':
             log.warn(f'Component {after} is not an Arm -- using MCPL file may cause problems')
@@ -363,7 +365,7 @@ class Instr:
                                fc.orientation, mcpl_output_parameters)
         first.components = first.components[:-1] + (mcpl_output,)
 
-        second.parameters += (InstrumentParameter('mcpl_filename', 'string', filename),)
+        second.add_parameter(InstrumentParameter('mcpl_filename', 'string', filename))
         mcpl_input_parameters = (ComponentParameter('filename', Expr.id('mcpl_filename')), )
         mcpl_input_comp = reader.get_component('MCPL_input')
         mcpl_input = Instance(fc.name, mcpl_input_comp, fc.at_relative, fc.rotate_relative,
--- test/test_instr.py
+++ test/test_instr.py
@@ -344,3 +344,30 @@ class TestInstr(TestCase):
         # Combining the position and rotation (reduced) operations should yield the same position
         comb = instr.components[2].orientation.combine().reduce()
         self.assertEqual(pos, comb.position())
+
+    def test_copy(self):
+        from mccode.instr import Instr
+        from mccode.common import InstrumentParameter, Expr, Value, DataType
+        instr_source = """
+        DEFINE INSTRUMENT test_copy(par0=3.14159, double par1 = 49, int par2 =     1010110
+            , string par3="this is a long string with spaces", 
+            
+            string par4, int par5, double par6, par7)
+        TRACE
+        COMPONENT first = Arm() AT (0, 0, 0) ABSOLUTE
+        COMPONENT second = Arm() AT (0, 0, 1) RELATIVE first ROTATED (0, 90, 0) RELATIVE first
+        END
+        """
+        instr = parse_instr_string(instr_source)
+        instr_copy = instr.copy()
+        self.assertTrue(isinstance(instr_copy, Instr))
+        self.assertEqual(instr.name, instr_copy.name)
+        self.assertEqual(len(instr.parameters), len(instr_copy.parameters))
+        self.assertEqual(len(instr.components), len(instr_copy.components))
+        for p, p_copy in zip(instr.parameters, instr_copy.parameters):
+            self.assertTrue(isinstance(p_copy, InstrumentParameter))
+            self.assertEqual(p.name, p_copy.name)
+            self.assertEqual(p.value, p_copy.value)
+
+        instr_copy.add_parameter(InstrumentParameter('par8', '', Expr.float(1.6189)))
+        self.assertEqual(len(instr.parameters), len(instr_copy.parameters) - 1)
