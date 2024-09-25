23f1cce69439618858749c75049f9bc7174dc293
Gregory Tucker <gregory.tucker@ess.eu>
2024-09-25 16:47:19 +0200

    [Ref] C block declare and typedef parsing
    In order to insert DECLARE variables into the component-type parameters
    struct, we need to be able to identify their type information.
    These types _could_ be user-defined `typedef` aliases, provided in,
    e.g., the SHARE section of the current or any-other component (probably
    preceding only, but possibly trailing definitions, since SHARE goes
    before all initialization).
    
    Previously parsing for both variable declaration and typedefs was
    performed in one pass, but this led to a confusing Listener class which
    mistakenly identified typenames for `typedef` `struct`s incorrectly,
    sometimes.
    E.g.,
    
    ```
    typdef struct the_struct_label {
        double a_double;
        int an_int;
    } the_struct_alias;
    ```
    would have been misidentified as the equivalent of
    `typedef struct the_struct_label an_int;`
    
    By splitting `typedef` and declaration parsing into two passes, there
    are now two slightly-simpler Listener classes that handle the above case
    plus all others implemented in a new test file.
    This double-pass invariably means slower parsing, but this seems a
    worthwhile trade-off at the moment.

--- tests/runtime/test_examples.py
+++ tests/runtime/test_examples.py
@@ -1,3 +1,6 @@
+from mccode_antlr.loader.loader import parse_mcstas_instr
+from textwrap import dedent
+
 from .compiled import compiled, compile_and_run
 
 @compiled
@@ -5,8 +8,6 @@ def test_template_instr():
     """
     This test failed previously because its name is a reserved word in C++ (template)
     """
-    from mccode_antlr.loader.loader import parse_mcstas_instr
-    from textwrap import dedent
     instr = parse_mcstas_instr(dedent("""\
     DEFINE INSTRUMENT template(Par1=1)
     DECLARE %{ %}
@@ -18,3 +19,22 @@ def test_template_instr():
     END
     """))
     compile_and_run(instr, "-n 1 Par1=2")
+
+
+@compiled
+def test_component_declare_variable_initialised():
+    instr = parse_mcstas_instr(dedent("""\
+    DEFINE INSTRUMENT namedsomething(dummy=0)
+    DECLARE %{
+    int initialized;
+    %}
+    INITIALIZE %{
+    initialized = -1;
+    %}
+    TRACE
+    component Origin = Progress_bar() at (0, 0, 0) absolute
+    component mirror = SupermirrorFlat() at (0, 0, 0) absolute
+    FINALLY %{ %}
+    END
+    """))
+    compile_and_run(instr, '-n 1 dummy=1')
