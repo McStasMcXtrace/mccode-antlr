fb151f2b0405d6e3b9872cf428fdab83c01fad55
Gregory Tucker <gregory.tucker@ess.eu>
2025-02-11 19:12:16 +0100

    [Fix] Windows macros are defined with /D not -D

--- tests/runtime/test_nexus.py
+++ tests/runtime/test_nexus.py
@@ -1,21 +1,18 @@
 from textwrap import dedent
 from mccode_antlr.loader import parse_mcstas_instr
 from .compiled import compiled_test, compile_and_run
-from mccode_antlr.config import config
 from mccode_antlr.reader.registry import InMemoryRegistry
 
-
-# config['flags']['acc'] = "-lm -fast -Minfo=accel -acc=host -DOPENACC -x c -D_POSIX_C_SOURCE=2"
 
 FAKE_COMPONENTS = dict(
     check_for_define=dedent(r"""DEFINE COMPONENT check_for_define
     SETTING PARAMETERS (int n)
     TRACE
     %{
-    #ifdef UNUSUAL_MACRO_CONTROL
-    printf("UNUSUAL_MACRO_CONTROL set\n");
+    #ifdef UNUSUAL_MACRO
+    printf("UNUSUAL_MACRO set\n");
     #else
-    printf("UNUSUAL_MACRO_CONTROL not set\n");
+    printf("UNUSUAL_MACRO not set\n");
     #endif
     %}
     END
@@ -30,6 +27,11 @@ FAKE_COMPONENTS = dict(
 )
 
 
+def macro_name():
+    from platform import system
+    return "/DUNUSUAL_MACRO" if system() == "Windows" else "-DUNUSUAL_MACRO"
+
+
 in_memory = InMemoryRegistry("test_components")
 for comp, repr in FAKE_COMPONENTS.items():
     in_memory.add_comp(comp, repr)
@@ -37,18 +39,18 @@ for comp, repr in FAKE_COMPONENTS.items():
 
 @compiled_test
 def test_instr_dependency():
-    instr = parse_mcstas_instr(dedent("""
+    instr = parse_mcstas_instr(dedent(f"""
     define instrument test_instr_dependency(dummy=0.)
-    DEPENDENCY "-DUNUSUAL_MACRO_CONTROL"
+    DEPENDENCY "{macro_name()}"
     trace
     component origin = check_for_define() at (0, 0, 0) absolute
     end
     """), registries=[in_memory])
-    assert any('-DUNUSUAL_MACRO_CONTROL' == x for x in instr.flags)
+    assert any(macro_name() == x for x in instr.flags)
     results, data = compile_and_run(instr, '-n 1 dummy=2')
     lines = results.decode('utf-8').splitlines()
-    assert sum('UNUSUAL_MACRO_CONTROL set' == x for x in lines) == 1
-    assert sum('UNUSUAL_MACRO_CONTROL not set' == x for x in lines) == 0
+    assert sum('UNUSUAL_MACRO set' == x for x in lines) == 1
+    assert sum('UNUSUAL_MACRO not set' == x for x in lines) == 0
 
 
 @compiled_test
@@ -59,11 +61,11 @@ def test_instr_no_dependency():
     component origin = check_for_define() at (0, 0, 0) absolute
     end
     """), registries=[in_memory])
-    assert not any('-DUNUSUAL_MACRO_CONTROL' == x for x in instr.flags)
+    assert not any(macro_name() == x for x in instr.flags)
     results, data = compile_and_run(instr, '-n 1 dummy=2')
     lines = results.decode('utf-8').splitlines()
-    assert sum('UNUSUAL_MACRO_CONTROL set' == x for x in lines) == 0
-    assert sum('UNUSUAL_MACRO_CONTROL not set' == x for x in lines) == 1
+    assert sum('UNUSUAL_MACRO set' == x for x in lines) == 0
+    assert sum('UNUSUAL_MACRO not set' == x for x in lines) == 1
 
 
 def test_nexus_key_is_expanded():
